5572d90f96bc97acf9017ecf266ad44a118f185a
Darshik924 <darshikladhe182@gmail.com>
2026-03-19 16:07:22 +0000

    Navbar done

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { FaUser } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { SiDevbox } from "react-icons/si";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import Logos from "./Logos.tsx";
 
 const Nav = () => {
   const navItemStyle = `text-3xl font-bold cursor-pointer hover:bg-amber-800 hover:text-amber-200 transition duration-150 text-white font-sans flex gap-2 p-3 bg-blue-950 rounded-2xl jelly-button`;
@@ -10,8 +13,8 @@ const Nav = () => {
 
   return (
     <nav className="h-20 w-full bg-blue-400 fixed top-0 left-0 z-50">
-      <div className="flex justify-between px-10 items-center h-full">
-        <ul className="flex gap-12">
+      <div className="flex justify-between px-24 items-center h-full">
+        <ul className="flex gap-14">
           <li className={navItemStyle}>
             <FaUser className={navIconStyles} />
             About
@@ -24,11 +27,9 @@ const Nav = () => {
             <IoIosMail className={navIconStyles} />
             Contact
           </li>
-          {/* <li className={navItemStyle}>Projects</li>
-          <li className={navItemStyle}>Contact</li> */}
         </ul>
 
-        <div className="w-10"></div>
+        <Logos />
       </div>
     </nav>
   );
