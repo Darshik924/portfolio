09310c8a9648c2e025b56cba29b5d36e6953112b
Darshik <darshikladhe182@gmail.com>
2026-03-21 21:21:52 +0530

    Changed Colours

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,7 +10,7 @@ const Nav = () => {
   const navIconStyles = `font-bold text-white h-7 w-7 mt-1`;
 
   return (
-    <nav className="h-20 w-full bg-blue-400 fixed top-0 left-0 z-50">
+    <nav className="h-20 w-full bg-blue-500 fixed top-0 left-0 z-50">
       <div className="flex justify-between px-24 items-center h-full">
         <ul className="flex gap-14">
           <li>
