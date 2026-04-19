216d04cfbaaf7652a919f3e20eb49b7ad2d6286f
Darshik <darshikladhe182@gmail.com>
2026-04-19 15:09:17 +0530

    Update Projects

--- src/db/Projects.ts
+++ src/db/Projects.ts
@@ -19,28 +19,6 @@ const projects: projectsType = [
     link: "#",
     github: "https://github.com/naresh-coder123/url-shortner.git",
   },
-  {
-    title: "Live Upvote Gallery",
-    flag: {
-      text: "(Group Project)",
-      color: "#00cb00",
-    },
-    desc: "A real-time image gallery where users can upload images and upvote their favorites — with live vote counts that update instantly across all connected devices, no refresh needed. Worked on the complete backend,frontend architecture and client sevrer communication",
-    tech: ["React", "Node.js", "MongoDB", "Express", "Socket.IO"],
-    link: "https://live-upvote-gallery.vercel.app/",
-    github: "https://github.com/Darshik924/live-upvote-gallery.git",
-  },
-  {
-    title: "ShopEase: E-commerce App",
-    flag: {
-      text: "(In Progress)",
-      color: "#ed0000",
-    },
-    desc: "A complete stack E-commerce website that has admin roles and customer roles for buyers and sellers",
-    tech: ["React", "Node.js", "MongoDB", "Express", "TypeScirpt"],
-    link: "#",
-    github: "https://github.com/Darshik924/shopease-e-commerce.git",
-  },
 ];
 
 export { projects };
